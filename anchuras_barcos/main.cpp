af14cd5f3246eac925dc4f0689231802eacd34bc
Pedro <simarroguerrapedro@gmail.com>
2020-02-09 20:15:33 +0100

    comentarios islas_pd

--- anchuras_barcos/main.cpp
+++ anchuras_barcos/main.cpp
@@ -2,6 +2,15 @@
 #include <algorithm>
 using namespace std;
 
+
+
+/*Ejercicio 7 Un archipiélago consta de unas cuantas islas y varios puentes que unen ciertos pares de
+islas entre sí. Para cada puente (que puede ser de dirección única o doble), además de saber la isla
+de origen y la de destino, se conoce su anchura > 0. La anchura de un camino formado por una
+sucesión de puentes es la anchura mínima entre las anchuras de los puentes que lo forman. Para
+cada isla, se desea saber cuál es el camino de anchura máxima que las une (siempre que exista
+alguno).*/
+
 const int N = 5;
 int G[N][N] = {
     {0, 20, 0, 3, 9},
@@ -24,6 +33,7 @@ int main(){
 
 void maxAnchura_pd(){
     
+    //Inicializamos las tablas
     for(int i = 0; i < N; ++i){
         for(int j = 0; j < N; ++j){
             anch[i][j] = G[i][j];
@@ -32,6 +42,7 @@ void maxAnchura_pd(){
         }
     }
 
+    //Rellenamos la tablas
     for(int k = 0; k < N; k++){
         for(int i = 0; i < N; ++i){
             for(int j = 0; j < N; ++j){
